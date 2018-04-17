ed389d7dc3eec055cd6b95e891a2eb0fd2094115
Anton Ukhanev <xedin.unknown@gmail.com>
2018-04-17 16:59:28 +0200

    `ConfigInterface` now expected to expose a key separator

--- test/unit/ConfigInterfaceTest.php
+++ test/unit/ConfigInterfaceTest.php
@@ -48,4 +48,16 @@ class ConfigInterfaceTest extends TestCase
         $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the test subject could not be created');
         $this->assertInstanceOf('Psr\Container\ContainerInterface', $subject, 'A valid instance of the test subject could not be created');
     }
+
+    /**
+     * Tests that the config key separator is correct.
+     *
+     * @since [*next-version*]
+     */
+    public function testKeySeparator()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertEquals('/', constant(sprintf('%1$s::%2$s', get_class($subject),'KEY_SEPARATOR')), 'Wrong key separator retrieved');
+    }
 }
